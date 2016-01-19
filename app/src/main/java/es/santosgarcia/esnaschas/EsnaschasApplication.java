11dbe33cf05652034e3c11e8358f8beb0c3cf9fb
SantosGarcia <santos@santos.com>
2016-01-19 20:20:44 +0100

    Funcionando mensajes de error en el boton send

--- app/src/main/java/es/santosgarcia/esnaschas/EsnaschasApplication.java
+++ app/src/main/java/es/santosgarcia/esnaschas/EsnaschasApplication.java
@@ -12,9 +12,6 @@ public class EsnaschasApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
-        // [Optional] Power your app with Local Datastore. For more info, go to
-        // https://parse.com/docs/android/guide#local-datastore
-       Parse.enableLocalDatastore(this);
         Parse.initialize(this);
     }
 }
--- app/src/main/java/es/santosgarcia/esnaschas/SingUpActivity.java
+++ app/src/main/java/es/santosgarcia/esnaschas/SingUpActivity.java
@@ -1,13 +1,87 @@
 package es.santosgarcia.esnaschas;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SingUpActivity extends AppCompatActivity {
+
+    private EditText Usernamefield;
+    private EditText Emailfield;
+    private EditText Passwordfield;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
+
+
+        Button send = (Button)findViewById(R.id.ButtonConfirm);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSend();
+            }
+        });
     }
+
+ protected void onSend(){
+
+         EditText Usenamefield = (EditText)findViewById(R.id.Usernamefield);
+         EditText Emailfield = (EditText)findViewById(R.id.Emailfield);
+         EditText Passwordfield = (EditText)findViewById(R.id.Passwordfield);
+
+        String errortittle = getString(R.string.error_tittle_message);
+        AlertDialog.Builder builder = new AlertDialog.Builder(SingUpActivity.this);
+
+        String tUsr="";
+        String tEmail="";
+        String tPass="";
+
+        String sUserField = getString(R.string.field_User);
+         tUsr = Usenamefield.getText().toString();
+
+        String sEmailField = getString(R.string.field_Email);
+         tEmail = Emailfield.getText().toString();
+
+        String sPassField = getString(R.string.field_Pass);
+         tPass = Passwordfield.getText().toString();
+
+        String message = null;
+
+        if (tUsr.trim().isEmpty()){
+            message = String.format(getString(R.string.empty_field_message),sUserField);
+        }
+        else
+            tUsr = String.valueOf(Usernamefield.getText());
+
+        if (tEmail.trim().isEmpty()){
+            message = String.format(getString(R.string.empty_field_message),sEmailField);
+        }
+        else
+            tEmail = String.valueOf(Emailfield.getText());
+
+        if (tPass.trim().isEmpty()){
+           message = String.format(getString(R.string.empty_field_message),sPassField);
+        }
+        else {
+            tPass = String.valueOf(Passwordfield.getText());
+        }
+
+
+         builder.setMessage(message);
+         builder.setTitle(errortittle);
+         builder.setPositiveButton(android.R.string.ok, null);
+         builder.setIcon(android.R.drawable.ic_dialog_alert);
+
+         AlertDialog dialog = builder.create();
+         dialog.show();
+
+    }
+
+
 }
