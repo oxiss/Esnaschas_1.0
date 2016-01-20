e979715155d836d9d36cab36ab4806b7649cf6aa
SantosGarcia <santos@santos.com>
2016-01-20 16:44:55 +0100

    Funcionando registro

--- app/src/main/java/es/santosgarcia/esnaschas/SingUpActivity.java
+++ app/src/main/java/es/santosgarcia/esnaschas/SingUpActivity.java
@@ -1,5 +1,6 @@
 package es.santosgarcia.esnaschas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -7,12 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 public class SingUpActivity extends AppCompatActivity {
-
-    private EditText Usernamefield;
-    private EditText Emailfield;
-    private EditText Passwordfield;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +31,7 @@ public class SingUpActivity extends AppCompatActivity {
 
  protected void onSend(){
 
-         EditText Usenamefield = (EditText)findViewById(R.id.Usernamefield);
+         EditText Usernamefield = (EditText)findViewById(R.id.Usernamefield);
          EditText Emailfield = (EditText)findViewById(R.id.Emailfield);
          EditText Passwordfield = (EditText)findViewById(R.id.Passwordfield);
 
@@ -43,7 +43,7 @@ public class SingUpActivity extends AppCompatActivity {
         String tPass="";
 
         String sUserField = getString(R.string.field_User);
-         tUsr = Usenamefield.getText().toString();
+         tUsr = Usernamefield.getText().toString();
 
         String sEmailField = getString(R.string.field_Email);
          tEmail = Emailfield.getText().toString();
@@ -53,34 +53,57 @@ public class SingUpActivity extends AppCompatActivity {
 
         String message = null;
 
-        if (tUsr.trim().isEmpty()){
-            message = String.format(getString(R.string.empty_field_message),sUserField);
+        if (tPass.isEmpty()){
+         message = String.format(getString(R.string.empty_field_message),sPassField);
         }
         else
-            tUsr = String.valueOf(Usernamefield.getText());
+            tPass = String.valueOf(Passwordfield.getText());
 
-        if (tEmail.trim().isEmpty()){
+
+        if (tEmail.isEmpty()){
             message = String.format(getString(R.string.empty_field_message),sEmailField);
         }
         else
             tEmail = String.valueOf(Emailfield.getText());
 
-        if (tPass.trim().isEmpty()){
-           message = String.format(getString(R.string.empty_field_message),sPassField);
+         if (tUsr.isEmpty()){
+         message = String.format(getString(R.string.empty_field_message),sUserField);
+         }
+         else
+            tUsr = String.valueOf(Usernamefield.getText());
+
+
+        if (tUsr.trim().isEmpty()|tEmail.trim().isEmpty()|tPass.trim().isEmpty()) {
+            builder.setMessage(message);
+            builder.setTitle(errortittle);
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setIcon(android.R.drawable.ic_dialog_alert);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
         }
-        else {
-            tPass = String.valueOf(Passwordfield.getText());
+        else{
+            SignUp(tUsr.trim(), tEmail.trim(), tPass.trim());
         }
+    }
 
+    private void SignUp(String name, String email, String pass) {
+        ParseUser user = new ParseUser();
+        user.setUsername(name);
+        user.setPassword(pass);
+        user.setEmail(email);
 
-         builder.setMessage(message);
-         builder.setTitle(errortittle);
-         builder.setPositiveButton(android.R.string.ok, null);
-         builder.setIcon(android.R.drawable.ic_dialog_alert);
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    Intent intent = new Intent(SingUpActivity.this,MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                } else {
 
-         AlertDialog dialog = builder.create();
-         dialog.show();
-
+                }
+            }
+        });
     }
 
 
