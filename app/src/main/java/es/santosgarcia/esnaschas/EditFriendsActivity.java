c28440d33a5dfff6abb31d5981dde4a1868d54c2
SantosGarcia <santos@santos.com>
2016-02-02 19:24:19 +0100

    Funcionando lista de amigos

--- app/src/main/java/es/santosgarcia/esnaschas/EditFriendsActivity.java
+++ app/src/main/java/es/santosgarcia/esnaschas/EditFriendsActivity.java
@@ -1,7 +1,9 @@
 package es.santosgarcia.esnaschas;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -9,22 +11,33 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 // Punto 14
 
 
-public class EditFriendsActivity extends AppCompatActivity {
+public class EditFriendsActivity extends ListActivity {
+
+    List<ParseUser> mUsers;
+    ArrayList<String> usernames;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ProgressBar mibarra = (ProgressBar) findViewById(R.id.progressBar2);
+        //mibarra.setVisibility(View.VISIBLE);
         setContentView(R.layout.activity_edit_friends);
     }
     @Override
     protected void onResume() {
         super.onResume();
+
+        usernames=new ArrayList<String>();
+        adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_checked,usernames);
+        setListAdapter(adapter);
         ParseQuery query = ParseUser.getQuery();
         query.orderByAscending(ParseConstants.KEY_USERNAME);
         query.setLimit(ParseConstants.MAX_USERS);
@@ -35,6 +48,10 @@ public class EditFriendsActivity extends AppCompatActivity {
             public void done(List<ParseUser> users, ParseException e) {
                 if (e == null) {
                     //succes
+                    mUsers=users;
+                    for(ParseUser user:mUsers){
+                        adapter.add(user.getUsername());
+                    }
                 } else {
                     Toast toast1 = Toast.makeText(getApplicationContext(),"EEEEEEERROR", Toast.LENGTH_SHORT);
                     toast1.show();
